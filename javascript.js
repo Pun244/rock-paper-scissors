317c6bf457d5b962697ebf7bce9393f46d3beb04
Pun224 <167857512+Pun244@users.noreply.github.com>
2024-05-10 20:19:16 +0800

    Add playRound and playGame funcion

--- javascript.js
+++ javascript.js
@@ -3,14 +3,75 @@ let computerScore = 0
 let playedRound = 0
 
 function getComputerChoice() {
-    let rps = ["Rock","Paper","Scissors"]
-    let randomInput = Math.floor(Math.random()* rps.length)
-    return rps[randomInput]
+    let rps = ["rock","paper","scissors"];
+    let randomInput = Math.floor(Math.random()* rps.length);
+    return rps[randomInput];
 }
-
-let input = prompt(`Input : "Rock","Paper","Scissors".`)
 
 function getUserInput() {
-    return input.charAt(0).toUpperCase() + input.slice(1).toLowerCase()
+    let userInput = prompt(`Input : "Rock","Paper" or "Scissors".`);
+    let inputItem = userInput.toLowerCase()
+    return inputItem
 }
-console.log(getUserInput())
+
+function playGame() {
+    let humanScore = 0
+    let computerScore = 0
+    let playedRound = 0
+
+    for (let i = 0; i < 5; i++) {
+        const humanSelection = getUserInput();
+        const computerSelection = getComputerChoice();
+        playRound(humanSelection, computerSelection);
+    }
+
+    function playRound(humanChoice, computerChoice) {
+        playedRound++
+        if (humanChoice === computerChoice) {
+            console.log(`User: '${humanChoice}' CPU: '${computerChoice}'`);
+            console.log("Tie, Try again!");
+            console.log(`Round: '${playedRound}/5' Your Score: '${humanScore}' CPU Score: '${computerScore}'`);
+        } else if (humanChoice === "rock" && computerChoice === "scissors") {
+            console.log(`User: '${humanChoice}' CPU: '${computerChoice}'`);
+            console.log("You win!");
+            console.log(`Round: '${playedRound}/5' Your Score: '${humanScore}' CPU Score: '${computerScore}'`);
+            ++humanScore;
+        } else if (humanChoice === "paper" && computerChoice === "rock")  {
+            console.log(`User: '${humanChoice}' CPU: '${computerChoice}'`);
+            console.log("You win!");
+            console.log(`Round: '${playedRound}/5' Your Score: '${humanScore}' CPU Score: '${computerScore}'`);
+            ++humanScore;
+        } else if (humanChoice === "scissors" && computerChoice === "paper") {
+            console.log(`User: '${humanChoice}' CPU: '${computerChoice}'`);
+            console.log("You win!");
+            console.log(`Round: '${playedRound}/5' Your Score: '${humanScore}' CPU Score: '${computerScore}'`);
+            ++humanScore;
+        } else if (humanChoice === "rock" && computerChoice === "paper") {
+            console.log(`User: '${humanChoice}' CPU: '${computerChoice}'`);
+            console.log("You lose!");
+            console.log(`Round: '${playedRound}/5' Your Score: '${humanScore}' CPU Score: '${computerScore}'`);
+            ++computerScore;
+        } else if (humanChoice === "paper" && computerChoice === "scissors") {
+            console.log(`User: '${humanChoice}' CPU: '${computerChoice}'`);
+            console.log("You lose!");
+            console.log(`Round: '${playedRound}/5' Your Score: '${humanScore}' CPU Score: '${computerScore}'`);
+            ++computerScore;
+        } else if (humanChoice === "scissors" && computerChoice === "rock") {
+            console.log(`User: '${humanChoice}' CPU: '${computerChoice}'`);
+            console.log("You lose!");
+            console.log(`Round: '${playedRound}/5' Your Score: '${humanScore}' CPU Score: '${computerScore}'`);
+            ++computerScore;
+        }
+    }
+
+    if (humanScore > computerScore) {
+        console.log(`You win!`)
+    } else if (computerScore > humanScore) {
+        console.log(`You lose!`)
+    } else {
+        console.log(`You Tied!`)
+    }
+
+}
+
+playGame()
